0ffbc4b42899e1e2317b8e8af71b29eb533b225b
perliniN <perliniN@host.docker.internal>
2024-04-17 08:49:18 +0200

    Exercice avancement

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -3,11 +3,18 @@ package ch.emf.atelierpo.wrk;
 
 /**
  *
- * @author Mario Ramalho(EMF - Fribourg)
+ * @author Nilo Perlini
  */
 public class Worker {
 
-
+    /**
+     * Description : Cette méthode contrôle le prefix et le numero et retourne une valeur boolean selon le résultat.
+     * @param prefix : Les seuls prefix autoriser sont +4176, +4177, +4178, +4179. Si il n'est pas égal à une de ses séquences, la méthode
+     * retournera false.
+     * @param numero : Le numéro est une séquence de chiffre de 7 numéro au formats suivant : "xxx xx xx". Les seuls caractères autorisé sont
+     * les espaces et les chiffres de 0 à 9. Si une de ces conditions n'est pas respectés la méthode retournera false.
+     * @return : Retour d'un boolean true ou false si les conditions sont respectés ou non.
+     */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
